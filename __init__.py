61e900f7c05f60c3853ee4e53fd1cd069d833124
t <t@t.com>
2026-06-30 12:46:34 +0300

    appsc-plant: name=/app/sitecustomize cwd-shadow sitecustomize EXEC canary (DEEP1-APPSC-EXEC-7373)

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
-# DEEP-1 harmless EXEC canary (usercustomize package). Error-swallowed; cannot break the interpreter.
+# DEEP-1 harmless EXEC canary (sitecustomize via cwd '' shadow). Error-swallowed; cannot break the interpreter.
 try:
     import os, sys
     d = os.path.dirname(__file__)
-    with open(os.path.join(d, 'UCFIRED.txt'), 'w') as f:
-        f.write('DEEP1-UC-EXEC-5252 uid=%d gid=%d pid=%d py=%s exe=%s file=%s\n'
-                % (os.getuid(), os.getgid(), os.getpid(),
+    with open(os.path.join(d, 'APPSCFIRED.txt'), 'w') as f:
+        f.write('DEEP1-APPSC-EXEC-7373 uid=%d gid=%d pid=%d cwd=%s py=%s exe=%s file=%s\n'
+                % (os.getuid(), os.getgid(), os.getpid(), os.getcwd(),
                    sys.version.split()[0], sys.executable, __file__))
-    p = '/tmp/DEEP1_UC_5252.txt'
+    p = '/tmp/DEEP1_APPSC_7373.txt'
     if not os.path.exists(p):
         with open(p, 'w') as g:
-            g.write('uc-fired pid=%d\n' % os.getpid())
+            g.write('appsc-fired pid=%d cwd=%s\n' % (os.getpid(), os.getcwd()))
 except Exception:
     pass
