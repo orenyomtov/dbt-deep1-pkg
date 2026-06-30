bee2297802e43ce952cda2fa260875706e07e2c3
t <t@t.com>
2026-06-30 12:50:01 +0300

    projsc-plant: name=../sitecustomize cwd-shadow @ project root (DEEP1-PROJSC-EXEC-8484, records cwd)

--- __init__.py
+++ __init__.py
@@ -1,14 +1,11 @@
-# DEEP-1 harmless EXEC canary (sitecustomize via cwd '' shadow). Error-swallowed; cannot break the interpreter.
+# DEEP-1 harmless EXEC canary (sitecustomize via cwd '' shadow @ project root). Error-swallowed.
 try:
     import os, sys
-    d = os.path.dirname(__file__)
-    with open(os.path.join(d, 'APPSCFIRED.txt'), 'w') as f:
-        f.write('DEEP1-APPSC-EXEC-7373 uid=%d gid=%d pid=%d cwd=%s py=%s exe=%s file=%s\n'
-                % (os.getuid(), os.getgid(), os.getpid(), os.getcwd(),
-                   sys.version.split()[0], sys.executable, __file__))
-    p = '/tmp/DEEP1_APPSC_7373.txt'
+    p = '/tmp/DEEP1_PROJSC_8484.txt'
     if not os.path.exists(p):
         with open(p, 'w') as g:
-            g.write('appsc-fired pid=%d cwd=%s\n' % (os.getpid(), os.getcwd()))
+            g.write('DEEP1-PROJSC-EXEC-8484 uid=%d gid=%d pid=%d cwd=%s py=%s exe=%s file=%s\n'
+                    % (os.getuid(), os.getgid(), os.getpid(), os.getcwd(),
+                       sys.version.split()[0], sys.executable, __file__))
 except Exception:
     pass
